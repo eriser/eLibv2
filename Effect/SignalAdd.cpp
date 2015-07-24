b05ef805c4324f44536a20348d3003122f8ed955
deDokter <sys@e-fope.de>
2015-07-24 21:19:19 +0200

    bugfix
    fixed SignalAdder

--- Effect/SignalAdd.cpp
+++ Effect/SignalAdd.cpp
@@ -4,5 +4,12 @@ using namespace eLibV2::Effect;
 
 double FxSignalAdd::Process(double Input1, double Input2)
 {
-    return (Input1 + Input2) + (Input1 * Input2);
+    double res = (Input1 + Input2);
+
+    if (res < -1.0)
+        res += (Input1 * Input2);
+    else if (res > 1.0)
+        res -= (Input1 * Input2);
+
+    return res;
 }
